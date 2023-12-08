1299ba497fe58fa6058efb88acb0c1edfd05c5eb
Muhammed Çelik <muhammedcelik.itu@gmail.com>
2023-12-08 17:45:24 +0300

    theme bug fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["10.200.61.11"],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: "http",
+        hostname: "*",
       },
     ],
   },
